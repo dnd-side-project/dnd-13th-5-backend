baa57d65776dcef088c8a6b6c571d972682695a9
Hwangseoeun <sunny030921@gmail.com>
2025-08-05 12:38:06 +0900

    feat: #1 Ci test 실패 테스트를 위해 추가

---
+++ src/test/java/com/dnd/sub/ExampleFailTest.java
@@ -0,0 +1,13 @@
+package com.dnd.sub;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ExampleFailTest {
+
+    @Test
+    void failTest() {
+        fail("의도적으로 실패시킨 테스트입니다.");
+    }
+}
