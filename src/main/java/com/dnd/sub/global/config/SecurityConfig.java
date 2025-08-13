7590274f082292a70a32fb2130feda588eca1caf
youcastle03 <luckthb325@naver.com>
2025-08-13 15:32:58 +0900

    feat:member entity

---
+++ src/main/java/com/dnd/sub/global/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.dnd.sub.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configurable
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+}
