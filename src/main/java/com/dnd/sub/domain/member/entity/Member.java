2ab6db26cbd57caa986967afb1762bcddcc49c43
youcastle03 <luckthb325@naver.com>
2025-08-13 22:54:09 +0900

    feat:jpaAuditing

--- src/main/java/com/dnd/sub/domain/member/entity/Member.java
+++ src/main/java/com/dnd/sub/domain/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.dnd.sub.domain.member.entity;
 
+import com.dnd.sub.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,7 +16,7 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/dnd/sub/global/config/SecurityConfig.java
+++ src/main/java/com/dnd/sub/global/config/SecurityConfig.java
@@ -1,16 +1,50 @@
-package com.dnd.sub.global.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configurable
-@RequiredArgsConstructor
-@EnableWebSecurity
-public class SecurityConfig {
-
-
-}
+//package com.dnd.sub.global.config;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Configurable;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//@Configurable
+//@RequiredArgsConstructor
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    http
+//        .cors(cors -> cors.configurationSource(configurationSource()))
+//        .csrf(AbstractHttpConfigurer::disable)
+//        .formLogin(AbstractHttpConfigurer::disable)
+//        .httpBasic(AbstractHttpConfigurer::disable)
+//        .sessionManagement(s -> s.sessionCreationPolicy((SessionCreationPolicy.STATELESS)))
+//        .authorizeHttpRequests(
+//            a -> a.requestMatchers("/").permitAll(); //.anyRequest().authenticated());
+/// /
+//    return http.build();
+//  }
+//
+//  @Bean
+//  public CorsConfigurationSource configurationSource() {
+//    CorsConfiguration configuration = new CorsConfiguration();
+//    configuration.setAllowedOriginPatterns(List.of("*"));
+//    configuration.setAllowedMethods(List.of("*"));
+//    configuration.setAllowedHeaders(List.of("*"));
+//    configuration.setAllowCredentials(true);
+//
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", configuration);
+//    return source;
+//  }
+//}
