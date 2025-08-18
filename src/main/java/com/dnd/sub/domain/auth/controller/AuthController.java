62b53f4b64ba1ad79ecc3d1ef223479c6b1bc60f
youcastle03 <luckthb325@naver.com>
2025-08-18 17:13:45 +0900

    feat:yml

--- src/main/java/com/dnd/sub/domain/auth/controller/AuthController.java
+++ src/main/java/com/dnd/sub/domain/auth/controller/AuthController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
