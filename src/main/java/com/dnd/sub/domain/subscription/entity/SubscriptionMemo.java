0ed118adbb4fc35d39a7d10c0422a9ba06bd1533
Hwangseoeun <sunny030921@gmail.com>
2025-08-19 22:05:54 +0900

    feat: #9 Subsciption entity 세팅

---
+++ src/main/java/com/dnd/sub/domain/subscription/entity/SubscriptionMemo.java
@@ -0,0 +1,37 @@
+package com.dnd.sub.domain.subscription.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "subscription")
+@Entity
+public class SubscriptionMemo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "subscription_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Subscription subscription;
+
+    @Column(name = "content", length = 200, nullable = false)
+    private String content;
+
+    public SubscriptionMemo(final Subscription subscription, final String content) {
+        this.subscription = subscription;
+        this.content = content;
+    }
+}
