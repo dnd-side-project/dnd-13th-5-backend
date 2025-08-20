b9f8f59da4e7371e974a48b8c904d550fb5a64c5
Hwangseoeun <sunny030921@gmail.com>
2025-08-21 00:53:44 +0900

    feat: #15 결제 주기 컬럼 분리

--- src/main/java/com/dnd/sub/domain/subscription/entity/Subscription.java
+++ src/main/java/com/dnd/sub/domain/subscription/entity/Subscription.java
@@ -5,8 +5,9 @@ import com.dnd.sub.domain.paymentmethod.entity.PaymentMethod;
 import com.dnd.sub.domain.product.entity.Product;
 import com.dnd.sub.global.entity.BaseEntity;
 import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -52,9 +53,12 @@ public class Subscription extends BaseEntity {
     @Column(name = "pay_type", length = 20, nullable = false)
     private String payType;
 
-    @Convert(converter = PayCycleConverter.class)
-    @Column(name = "pay_cycle", length = 20, nullable = false)
-    private PayCycle payCycle;
+    @Column(name = "pay_cycle_num", nullable = false)
+    private int payCycleNum;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "pay_cycle_unit", length = 20, nullable = false)
+    private PayCycleUnitType payCycleUnit;
 
     @Column(name = "is_favorite", columnDefinition = "TINYINT(1)", nullable = false)
     private boolean isFavorite = false;
@@ -71,7 +75,8 @@ public class Subscription extends BaseEntity {
         final LocalDate startedAt,
         final int participantCount,
         final String payType,
-        final PayCycle payCycle
+        final int payCycleNum,
+        final PayCycleUnitType payCycleUnit
     ) {
         this.member = member;
         this.product = product;
@@ -79,6 +84,7 @@ public class Subscription extends BaseEntity {
         this.startedAt = startedAt;
         this.participantCount = participantCount;
         this.payType = payType;
-        this.payCycle = payCycle;
+        this.payCycleNum = payCycleNum;
+        this.payCycleUnit = payCycleUnit;
     }
 }
