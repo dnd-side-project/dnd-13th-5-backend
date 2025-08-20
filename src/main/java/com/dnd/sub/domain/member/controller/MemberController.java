4dcbec9743ffd65c81138fc977b84c840608d8cc
Hwangseoeun <sunny030921@gmail.com>
2025-08-21 00:53:20 +0900

    feat: #15 로컬 환경에서 테스트를 위한 JWT Token 발급 api 생성

--- src/main/java/com/dnd/sub/domain/member/controller/MemberController.java
+++ src/main/java/com/dnd/sub/domain/member/controller/MemberController.java
@@ -5,11 +5,14 @@ import com.dnd.sub.domain.member.dto.response.MemberInfoResponse;
 import com.dnd.sub.domain.member.dto.response.MemberSuccessCode;
 import com.dnd.sub.domain.member.service.MemberService;
 import com.dnd.sub.global.dto.ApiResponse;
+import com.dnd.sub.global.security.jwt.JwtProvider;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController implements MemberControllerDocs {
 
     private final MemberService memberService;
+    private final JwtProvider jwtProvider;
 
     @GetMapping("/my/info")
     public ApiResponse<MemberInfoResponse> getMemberInfo(@AuthenticationPrincipal Long memberId) {
@@ -34,4 +38,10 @@ public class MemberController implements MemberControllerDocs {
         return ApiResponse.success(MemberSuccessCode.MEMBER_INFO_UPDATE, response);
     }
 
+    //local 환경에서 테스트 시에만 사용
+    @Profile("local")
+    @GetMapping("/{memberId}")
+    public String generateJwtToken(@PathVariable Long memberId) {
+        return jwtProvider.generateToken(memberId);
+    }
 }
