02c604019a1e72e75648150da56f8c34b6fcf36e
youcastle03 <luckthb325@naver.com>
2025-08-20 21:40:15 +0900

    fix:오탈자 수정

--- src/main/java/com/dnd/sub/domain/member/dto/response/MemberInfoResponse.java
+++ src/main/java/com/dnd/sub/domain/member/dto/response/MemberInfoResponse.java
@@ -3,6 +3,6 @@ package com.dnd.sub.domain.member.dto.response;
 public record MemberInfoResponse(
     String email,
     String name,
-    boolean isNotificaionOn
+    boolean isNotificationOn
 ) {
 }
