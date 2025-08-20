59efaebcd33e7796a96d8a36929a0fa42088550e
youcastle03 <luckthb325@naver.com>
2025-08-20 13:21:39 +0900

    feat:MemberSuccessCode사용

--- src/main/java/com/dnd/sub/domain/member/controller/MemberController.java
+++ src/main/java/com/dnd/sub/domain/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.dnd.sub.domain.member.controller;
 
 import com.dnd.sub.domain.member.dto.response.MemberInfoResponse;
+import com.dnd.sub.domain.member.dto.response.MemberSuccessCode;
 import com.dnd.sub.domain.member.service.MemberService;
 import com.dnd.sub.global.dto.ApiResponse;
 import com.dnd.sub.global.enums.GlobalSuccessCode;
@@ -8,6 +9,7 @@ import com.dnd.sub.global.enums.SuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,13 @@ public class MemberController {
 
     @GetMapping("/my/info")
     public ApiResponse<MemberInfoResponse> getMemberInfo(@AuthenticationPrincipal Long memberId) {
+        MemberInfoResponse response = memberService.getMemberInfo(memberId);
+        return ApiResponse.success(MemberSuccessCode.MEMBER_INFO_OK, response);
+    }
+
+    @PatchMapping("/my/info")
+    public ApiResponse<MemberInfoResponse> updateMemberInfo(
+        @AuthenticationPrincipal Long memberId) {
         MemberInfoResponse response = memberService.getMemberInfo(memberId);
         return ApiResponse.success(GlobalSuccessCode.OK, response);
     }
---
+++ src/main/java/com/dnd/sub/domain/member/dto/response/MemberSuccessCode.java
@@ -0,0 +1,19 @@
+package com.dnd.sub.domain.member.dto.response;
+
+import com.dnd.sub.global.enums.SuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberSuccessCode implements SuccessCode {
+    MEMBER_INFO_OK("MIS-201", HttpStatus.OK, "사용자 정보 조회 성공"),
+    MEMBER_INFO_UPDATE("MIS-202", HttpStatus.OK, "이메일 변경 완료");
+
+
+    private final String code;
+    private final HttpStatus status;
+    private final String message;
+
+}
