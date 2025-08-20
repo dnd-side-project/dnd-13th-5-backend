71b883d9260be1216e2264e65c617c74ed31b529
Hwangseoeun <sunny030921@gmail.com>
2025-08-20 13:58:30 +0900

    feat: #9 repository 생성

---
+++ src/main/java/com/dnd/sub/domain/subscription/repository/SubscriptionRepository.java
@@ -0,0 +1,7 @@
+package com.dnd.sub.domain.subscription.repository;
+
+import com.dnd.sub.domain.subscription.entity.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+}
