457ed6eda5a9ae291660685ca36c9f6714675f44
Hwangseoeun <sunny030921@gmail.com>
2025-08-21 12:17:30 +0900

    feat: #15 HttpStatus 타입 변경

--- src/main/java/com/dnd/sub/global/enums/GlobalErrorCode.java
+++ src/main/java/com/dnd/sub/global/enums/GlobalErrorCode.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum GlobalErrorCode implements ErrorCode {
-    NOT_FOUND("임시 코드", HttpStatus.NOT_FOUND, "값을 찾을 수 없습니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND.value(), "임시 코드", "값을 찾을 수 없습니다.");
 
 
+    private final int status;
     private final String code;
-    private final HttpStatus status;
     private final String message;
 }
