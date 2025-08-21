07be078a1f10dfd53eae270dc296f72e85285f9d
Hwangseoeun <sunny030921@gmail.com>
2025-08-21 11:54:43 +0900

    feat: #15 jwt token 발급 메서드 패키지 이동

--- src/main/java/com/dnd/sub/domain/member/controller/MemberController.java
+++ src/main/java/com/dnd/sub/domain/member/controller/MemberController.java
@@ -5,14 +5,11 @@ import com.dnd.sub.domain.member.dto.response.MemberInfoResponse;
 import com.dnd.sub.domain.member.dto.response.MemberSuccessCode;
 import com.dnd.sub.domain.member.service.MemberService;
 import com.dnd.sub.global.dto.ApiResponse;
-import com.dnd.sub.global.security.jwt.JwtProvider;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController implements MemberControllerDocs {
 
     private final MemberService memberService;
-    private final JwtProvider jwtProvider;
 
     @GetMapping("/my/info")
     public ApiResponse<MemberInfoResponse> getMemberInfo(@AuthenticationPrincipal Long memberId) {
@@ -43,12 +39,5 @@ public class MemberController implements MemberControllerDocs {
         @AuthenticationPrincipal Long memberId) {
         MemberInfoResponse response = memberService.updateNotificationStatus(memberId);
         return ApiResponse.success(MemberSuccessCode.MEMBER_NOTI_UPDATE, response);
-    }
-
-    //local 환경에서 테스트 시에만 사용
-    @Profile("local")
-    @GetMapping("/{memberId}")
-    public String generateJwtToken(@PathVariable Long memberId) {
-        return jwtProvider.generateToken(memberId);
     }
 }
