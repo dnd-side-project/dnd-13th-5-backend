dcfe85ed2fccb16b498f4d72efa6188854976a74
Hwangseoeun <sunny030921@gmail.com>
2025-08-22 15:52:34 +0900

    feat: #29 error code 수정

--- src/main/java/com/dnd/sub/domain/subscription/exception/SubscriptionErrorCode.java
+++ src/main/java/com/dnd/sub/domain/subscription/exception/SubscriptionErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum SubscriptionErrorCode implements ErrorCode {
-    SUBSCRIPTION_ERROR(HttpStatus.BAD_REQUEST.value(), "SF-401", "사용자가 구독중인 서비스에 예기치 못한 문제가 발생했습니다."),
+    SUBSCRIPTION_ERROR(HttpStatus.BAD_REQUEST.value(), "SF-400", "사용자가 구독중인 서비스에 예기치 못한 문제가 발생했습니다."),
     ;
 
     private final int status;
