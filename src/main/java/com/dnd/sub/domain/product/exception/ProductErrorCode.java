456385aa8fbcbb42b9c1be7c6ab6c19ac004d24d
Hwangseoeun <sunny030921@gmail.com>
2025-08-22 13:57:55 +0900

    feat: #27 error 코드 수정

--- src/main/java/com/dnd/sub/domain/product/exception/ProductErrorCode.java
+++ src/main/java/com/dnd/sub/domain/product/exception/ProductErrorCode.java
@@ -3,10 +3,13 @@ package com.dnd.sub.domain.product.exception;
 import com.dnd.sub.global.enums.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum ProductErrorCode implements ErrorCode {
+
+    PRODUCT_ERROR(HttpStatus.BAD_REQUEST.value(), "PF-401", "구독 서비스에 예기치 못한 문제가 발생했습니다."),
     ;
 
     private final int status;
--- src/main/java/com/dnd/sub/domain/subscription/exception/SubscriptionErrorCode.java
+++ src/main/java/com/dnd/sub/domain/subscription/exception/SubscriptionErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum SubscriptionErrorCode implements ErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND.value(), "MF-401", "해당 유저(id)를 찾을 수 없습니다.");
+    SUBSCRIPTION_ERROR(HttpStatus.BAD_REQUEST.value(), "SF-401", "사용자가 구독중인 서비스에 예기치 못한 문제가 발생했습니다."),
+    ;
 
     private final int status;
     private final String code;
