aeae75b0fc57367f1c7b1b0393ba147535f699f7
Hwangseoeun <sunny030921@gmail.com>
2025-08-22 15:38:02 +0900

    feat: #29 각 구독 서비스 별 요금제 목록 조회 API 구현

--- src/main/java/com/dnd/sub/domain/product/controller/ProductController.java
+++ src/main/java/com/dnd/sub/domain/product/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.dnd.sub.domain.product.controller;
 
+import com.dnd.sub.domain.product.dto.GetAllPlanOfProductDto;
 import com.dnd.sub.domain.product.dto.GetProductDto;
+import com.dnd.sub.domain.product.dto.response.GetAllPlanOfProductResponse;
 import com.dnd.sub.domain.product.dto.response.GetAllProductsResponse;
 import com.dnd.sub.domain.product.dto.response.GetProductCategoriesResponse;
 import com.dnd.sub.domain.product.entity.ProductCategoryType;
@@ -8,14 +10,14 @@ import com.dnd.sub.domain.product.service.ProductService;
 import com.dnd.sub.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import static com.dnd.sub.domain.product.dto.response.ProductSuccessCode.GET_ALL_PRODUCTS;
-import static com.dnd.sub.domain.product.dto.response.ProductSuccessCode.GET_PRODUCT_CATEGORIES;
+import static com.dnd.sub.domain.product.dto.response.ProductSuccessCode.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
@@ -38,5 +40,13 @@ public class ProductController {
         GetAllProductsResponse response = GetAllProductsResponse.from(products);
 
         return ApiResponse.success(GET_ALL_PRODUCTS, response);
+    }
+
+    @GetMapping("/{productId}/plans")
+    public ApiResponse<GetAllPlanOfProductResponse> getAllPlanOfProduct(@PathVariable Long productId) {
+        List<GetAllPlanOfProductDto> plans = productService.getAllPlanOfProduct(productId);
+        GetAllPlanOfProductResponse response = new GetAllPlanOfProductResponse(plans);
+
+        return ApiResponse.success(GET_ALL_PLAN_OF_PRODUCT, response);
     }
 }
