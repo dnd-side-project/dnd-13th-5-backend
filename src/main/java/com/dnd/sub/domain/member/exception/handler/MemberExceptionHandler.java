a214ccba1fa980a1911fbca440543892bb9ec50d
youcastle03 <luckthb325@naver.com>
2025-08-22 21:49:17 +0900

    refactor:MemberExceptionHandler분리

---
+++ src/main/java/com/dnd/sub/domain/member/exception/handler/MemberExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.dnd.sub.domain.member.exception.handler;
+
+import com.dnd.sub.domain.member.exception.MemberException;
+import com.dnd.sub.global.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
+public class MemberExceptionHandler {
+
+    @ExceptionHandler(MemberException.class)
+    protected ApiResponse<Void> handleMemberException(MemberException e) {
+        log.error("MemberException: {}", e.getErrorCode().getMessage());
+        return ApiResponse.fail(e.getErrorCode());
+    }
+}
--- src/main/java/com/dnd/sub/global/exception/GlobalExceptionController.java
+++ src/main/java/com/dnd/sub/global/exception/GlobalExceptionController.java
@@ -17,11 +17,4 @@ public class GlobalExceptionController {
         return ApiResponse.fail(e.getErrorCode());
     }
 
-
-    @ExceptionHandler(MemberException.class)
-    protected ApiResponse<Void> handleMemberException(MemberException e) {
-        log.error("MemberException: {}", e.getErrorCode().getMessage());
-        return ApiResponse.fail(e.getErrorCode());
-    }
-
 }
