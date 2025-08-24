9518fdfd8e56713bb877dec2d3be9fa0efdb7986
Hwangseoeun <sunny030921@gmail.com>
2025-08-25 01:51:02 +0900

    feat: #45 JPA 메서드 명 수정

--- src/main/java/com/dnd/sub/domain/subscription/repository/SubscriptionRepository.java
+++ src/main/java/com/dnd/sub/domain/subscription/repository/SubscriptionRepository.java
@@ -3,7 +3,12 @@ package com.dnd.sub.domain.subscription.repository;
 import com.dnd.sub.domain.subscription.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long>, SubscriptionQueryRepository {
 
-    boolean existsByMemberIdAndId(Long memberId, Long subscriptionId);
+    boolean existsByIdAndMember_Id(Long subscriptionId, Long memberId);
+
+    List<Subscription> findByNextPaymentDayBefore(LocalDate today);
 }
