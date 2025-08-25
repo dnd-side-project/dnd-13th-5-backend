c9b84a105f1b40e095f969e10a30a5a84270789b
youcastle03 <luckthb325@naver.com>
2025-08-25 19:40:54 +0900

    fix:오타수정

--- src/main/java/com/dnd/sub/global/util/CookieUtil.java
+++ src/main/java/com/dnd/sub/global/util/CookieUtil.java
@@ -11,7 +11,7 @@ public class CookieUtil {
     public static ResponseCookie createCookie(String key, String value, int maxAge) {
         return ResponseCookie.from(key, value)
             .maxAge(maxAge)
-            .sameSite("none")
+            .sameSite("None")
             .path("/")
             .httpOnly(true)
             .secure(false)
